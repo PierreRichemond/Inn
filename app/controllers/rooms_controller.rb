2cdf1840fcae7eb3fec10705828757dd06b1fe8c
= <pierreambroise@hotmail.fr>
2021-11-14 21:22:06 +0900

    fixed calendar on has_events class

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,10 +5,13 @@ class RoomsController < ApplicationController
 
     # Scope your query to the dates being shown:
     start_date = params.fetch(:start_date, Date.today).to_date
+
     # For a monthly view:
     @bookings = Booking.where(start_date: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+
     # Or, for a weekly view:
     @bookings = Booking.where(start_date: start_date.beginning_of_week..start_date.end_of_week)
+
   end
 
 
