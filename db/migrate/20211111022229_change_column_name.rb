f8b98c0e1752ef077ba16de2ef50863be86418e0
= <pierreambroise@hotmail.fr>
2021-11-23 00:30:09 +0900

    db dropped and migrated

--- db/migrate/20211111022229_change_column_name.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnName < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :bookings, :start_day, :start_date
-    rename_column :bookings, :end_day, :end_date
-  end
-end
