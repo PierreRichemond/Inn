9f7cc1abaef313250aea47ef3f13a028c859427a
= <pierreambroise@hotmail.fr>
2021-12-12 21:44:48 +0900

    removed initializer for act as tagable

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,15 +2,13 @@ class ActivitiesController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
-    @tags =
-    if params[:locale] == 'en'
-    Activity::TAGS.values_at(* Activity::TAGS.each_index.select {|i| i.odd?})
+    @tags = if params[:locale] == 'en'
+      Activity::TAGS.values_at(* Activity::TAGS.each_index.select {|i| i.odd?})
     else
-    Activity::TAGS.values_at(* Activity::TAGS.each_index.select {|i| i.even?})
+      Activity::TAGS.values_at(* Activity::TAGS.each_index.select {|i| i.even?})
     end
 
-    @activities =
-    if params[:tag].present?
+    @activities = if params[:tag].present?
       Activity.tagged_with(params[:tag])
     else
       Activity.all
