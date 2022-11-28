9b77897be018d0ecf5d4fb3af54bcda386dbb694
= <pierreambroise@hotmail.fr>
2022-11-28 15:48:33 +0100

    removed unnecessary lines

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,7 +1,6 @@
 # app/controllers/sitemaps_controller.rb
 
 class SitemapsController < ApplicationController
-
   layout :false
   before_action :init_sitemap
 
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,5 +4,4 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   monetize :amount_cents
-
 end
