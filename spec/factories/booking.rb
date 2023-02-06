f788a0bec383d6c5a1bd078b1bfd21b22133d3ee
= <pierreambroise@hotmail.fr>
2023-02-06 16:14:22 +0100

    added validation tests related to models: user, booking, booking preview

---
+++ spec/factories/booking.rb
@@ -0,0 +1,13 @@
+# spec/factories/booking.rb
+
+FactoryBot.define do
+  factory :booking do
+    state { "Past" }
+    room_name { "Ronsard" }
+    user_id { User.first || create(:user) }
+    room_id { Room.first || create(:room) }
+    start_date { 2.days.ago }
+    end_date { 1.days.ago }
+    amount_cents { 0 }
+  end
+end
---
+++ spec/models/booking_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe Booking, :type => :model do
+  describe "Check validation" do
+    context "when invalid" do
+      it "raises an error" do
+        expect(build(:booking, start_date: nil)).not_to be_valid
+        expect(build(:booking, end_date: nil)).not_to be_valid
+      end
+      it "Check for a valid factory" do
+        expect(build(:booking)).to be_valid
+      end
+    end
+  end
+end
