87cf6cbb0316c8df7297a278635d9f0031760be5
= <pierreambroise@hotmail.fr>
2023-02-15 10:21:43 +0100

    setup overlap, need review, SQL doesnt work

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,32 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   monetize :amount_cents
+
+  validate :cannot_overlap
+  validate :end_date_past_start_date
+
+  def end_date_past_start_date
+    return true unless (end_date && start_date) && (end_date <= start_date)
+
+    errors.add(:start_date, "Start date needs to be before end date.")
+  end
+
+  def cannot_overlap
+    return true unless overlap?
+
+    errors.add(:start_date, "Dates overlap with another booking, change your dates")
+  end
+
+  def overlap?
+    if id
+      return Booking.where("id != ?", id)
+                    .where("room_id = ?", room.id)
+                    .where("end_date >= #{start_date} AND start_date <= #{end_date}")
+                    .any?
+    end
+
+    Booking.where("room_id = ?", room.id)
+           .where("end_date >= #{start_date} AND start_date <= #{end_date}")
+           .any?
+  end
 end
